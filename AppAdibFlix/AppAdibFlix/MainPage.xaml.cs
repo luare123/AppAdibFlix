cf924e32f7bc40fa9d59635090242869a9da192f
Usuario <Usuario@CPD03-M15>
2022-06-08 13:59:03 -0300

    Adicionar arquivos de projeto.

---
+++ AppAdibFlix/AppAdibFlix/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppAdibFlix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnMorbius.Source = ImageSource.FromResource("AppAdibFlix.Posters.morbius.jpg");
+            
+        }
+
+        private void btnMorbius_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Categorias.Acao());
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+    }
+}
